4dcd819afae875dab663beecaf658d2d422354a8
AlexeyE1 <eremenkoalexey1@gmail.com>
2025-02-03 13:33:05 +0300

    adding debug toolbar

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'users',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'todolist.urls'
@@ -148,3 +150,8 @@ EMAIL_HOST_PASSWORD = 'ernm bujn eknb cwbc'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('users/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
-]
+] + debug_toolbar_urls()
