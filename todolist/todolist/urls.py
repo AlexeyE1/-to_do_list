67adc2116b187c6b81f3822fa00177c0bbcb7df6
Alexey <AlexeyEremenkoMain@yandex.ru>
2025-02-03 22:18:11 +0300

    added authentication via Google

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('users/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ] + debug_toolbar_urls()
